15272c366fe8a1222da25e4c063d64fe3de3a070
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2022-10-15 17:03:07 -0400

    Adding morgan package, css file, pug template

--- schedulo_app.js
+++ schedulo_app.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+app.use(express.static("public"));
+app.use(morgan("common"));
+
 app.get("/", (req, res) => {
-  res.render("schedulo-english");
+  res.render("schedulo-main");
 });
 
 app.listen(3000, "localhost", () => {
