eea862a1165371e257b4a448b561d6171c1cba0e
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2022-10-15 16:23:41 -0400

    Added nodemon (dev) and pug packages

--- schedulo_app.js
+++ schedulo_app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.send("Schedulo is here\n");
+  res.render("schedulo-english");
 });
 
 app.listen(3000, "localhost", () => {
