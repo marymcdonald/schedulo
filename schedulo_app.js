a06cd8508eb8fe0acb0c5fe69b8aec4b77d8ab01
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2022-10-16 15:09:46 -0400

    Add two paths (/home, /employees) and employees view

--- schedulo_app.js
+++ schedulo_app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+let {employees, weeks} = require("./lib/seed-data");
+const Schedulo = require("./lib/schedulo");
 const app = express();
+
+const schedule = new Schedulo(employees, weeks);
 
 app.set("views", "./views");
 app.set("view engine", "pug");
@@ -9,7 +13,17 @@ app.use(express.static("public"));
 app.use(morgan("common"));
 
 app.get("/", (req, res) => {
+  res.redirect("/home");
+});
+
+app.get("/home", (req, res) => {
   res.render("schedulo-main");
+})
+
+app.get("/employees", (req, res) => {
+  res.render("employees", {
+    employees: schedule.getAllEmployees(),
+  });
 });
 
 app.listen(3000, "localhost", () => {
